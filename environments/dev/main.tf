3b4036362cd4fa5a51cb7c23622860bd2f70deb7
Iam-mithran <martinmithran@gmail.com>
2025-05-27 15:08:22 +0530

    Changed cidr value

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source              = "../../modules/vpc"
   vpc_cidr            = "10.0.0.0/16"
-  subnet_cidr        = "10.0.1.0/24"
+  subnet_cidr        = "10.0.2.0/24"
   availability_zone  = "${var.aws_region}a"
   env                 = "dev"
 }
